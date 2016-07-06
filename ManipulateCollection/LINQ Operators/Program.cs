7de94b21eaef18759621ab4bbb205c087c192dd3
elena.balan <elena.balan@MDDSK40054>
2016-07-06 17:07:47 +0300

    Manipulate Collection and LINQ

---
+++ ManipulateCollection/LINQ Operators/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Operators
+{
+    class Products
+    {
+        public string name;
+        public string category;
+        public double price;
+        public int storeNumber;
+        int count;
+
+        public Products(string s, string c, double p, int nr, int i)
+        {
+            name = s;
+            category = c;
+            price = p;
+            storeNumber = i;
+            count = nr;
+        }
+
+        public override string ToString()
+        {
+            return $"{name,10}  {category,10}  price= {price,7}  nrStory= {storeNumber,2}  count= {count,5}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var criterii = new { category = "FURNITURE",
+                                 price = 500
+                                };
+
+            var prod = new List<Products> {
+                new Products ("table","FURNITURE",1547,12,1),
+                new Products("chair", "FURNITURE", 567, 52, 1),
+                new Products("TV","ELECTRIC",35000,5,1),
+                new Products("blender","ELECTRIC",980.50,15,2),
+                new Products("sofa","FURNITURE",14750,2,2),
+                new Products("Chips","FOOD",20.7,100,1) };
+
+            prod.Add(new Products("nuts", "FOOD", 5.67, 200, 2));  
+            prod.Add(new Products("bookcase", "FURNITURE", 276.5, 17, 1));
+
+            Console.WriteLine("Lista de produse initiala");
+            foreach (var p in prod)            
+                Console.WriteLine(p.ToString());
+
+            // QUERY
+            Console.WriteLine("\nUsing QUERY     Selectam toate produsele de tip FURNITURE\n");
+
+            var furniture = (from pr in prod
+                            where pr.category == criterii.category          //aici folosim un anonimous type CRITERII
+                             select pr.name +"  " + pr.price ).ToList();            //ToList() implica executarea imediata;
+
+            foreach (var p in furniture)            //fara ToList() executarea ar fi aminata pina a incepe iteratia data
+                Console.WriteLine(p);
+
+            ////FLUENT (LambdaSyntax)
+            Console.WriteLine($"\nUsing FLUENT (LAMBDASYNTAX)    \nSelectam produsele cu cost mai mare decit {criterii.price} si le afisam in ordinea de la mai mare la mai mic\n");
+
+            var hightPrice = prod.
+                            Where(p => p.price > criterii.price).      //aici folosim un anonimous type CRITERII
+                            OrderByDescending(p => p.price).       
+                            Select(p => p.name + " " + p.category + " " + p.price);
+             
+            foreach (var p in hightPrice)
+                Console.WriteLine(p);
+
+            Console.ReadKey();
+        }
+    }
+}
--- ManipulateCollection/ManipulateCollection/Program.cs
+++ ManipulateCollection/ManipulateCollection/Program.cs
@@ -6,10 +6,99 @@ using System.Threading.Tasks;
 
 namespace ManipulateCollection
 {
+    public delegate void MyDelegate(string s);
+    public delegate bool CompareDelegate(int i, string s);
+    public delegate string ConcatDelegate(int i, string s);
+
+    public static class ExtensionMethods
+    {
+        public static string UppercaseFirstLetter(this string ss)
+        {
+            // Uppercase the first letter in the string.
+
+            if (ss.Length > 0)
+            {
+                char[] array = ss.ToCharArray();
+                array[0] = char.ToUpper(array[0]);
+                return new string(array);
+            }
+            return ss;
+        }
+    }
+
     class Program
     {
+
+        public static void PrintConsoleUppercaseFirstLetter(string s)
+        {
+            if (s.Length > 0)
+            {
+                char[] array = s.ToCharArray();
+                array[0] = char.ToUpper(array[0]);
+                Console.Write(new string(array) + " ");
+            }
+
+        }
+        public static void Print(string s, MyDelegate del)
+        {
+            del(s);
+        }
+        public bool CompareAsString(int i, string s)   //this method can be associated to CompareDelegate
+        {
+            return (i.ToString() == s);
+        }
+
+
         static void Main(string[] args)
         {
+
+            List<string> words = new List<string> { "some", "words", "in", "this", "list" };
+
+            MyDelegate del1 = PrintConsoleUppercaseFirstLetter;
+            Console.WriteLine("\nDelegate1    Uppercase First Letter");
+            foreach (string s in words)
+                del1(s);
+
+            Console.WriteLine("\n****************");
+            Console.WriteLine("\nDelegate2    Write as a sentences");
+            MyDelegate del2 = s => Console.Write(s + "  ");
+            // or using 
+            //MyDelegate del2 = PrintWord;
+            foreach (string s in words)
+                del2(s);
+
+            Console.WriteLine("\n****************");
+            Console.WriteLine("\nDelegate1 + Delegate2");
+            MyDelegate del = del1 + del2;
+            foreach (string s in words)
+                del(s);
+            Console.WriteLine("\n****************");
+            Console.WriteLine("\nDifferent delegates on even and non-even position");
+            int poz = 0;
+            foreach (string s in words)
+            {
+                if (poz % 2 == 0)
+                    Print(s, del1);
+                else
+                    Print(s, del2);
+                poz++;
+            }
+
+            Console.WriteLine("\n****************");
+            Console.WriteLine("\nUsing of Anonimous function (in this exemple lambda expresion is Anonimous function)");
+            int ii = 8;
+            string ss = "2";
+            ConcatDelegate myDel = ((i, s) => i.ToString() + s);   //i.ToString() + s    -  this is an anonimous function 
+            Console.WriteLine($"Number {ii} + \"{ss}\" as string = \"{myDel(ii, ss)}\"");
+
+            //
+            // Use the string extension method on the first element of List WORDS
+            //
+
+            Console.WriteLine("\nUse the string extension method on the first element of List WORDS");
+            Console.WriteLine($"The words[0] = {words.First()}. After appling UppercaseFirstLetter result is   {words.First().UppercaseFirstLetter()}");
+
+            Console.ReadKey();
         }
     }
 }
